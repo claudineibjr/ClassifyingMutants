a793af273882759c81cf0d3ddeb0b5dbc4bc2fa8
ClaudineiBJr <claudineibjr@hotmail.com>
2019-11-19 13:38:09 -0300

    Inclusion of analysis of program-by-program classifier results

--- ML/analyzes.py
+++ ML/analyzes.py
@@ -642,6 +642,117 @@ def analyzeMetricsFromProgram(metricsFromProgram):
 
 	return programsBestMetrics
 
+def analyzeClassifiersProgramAProgram(metricsFromProgram, possibleClassifiers, plot=False):
+	'''
+		Function responsible to verify the classifiers and indicate the results program by program
+	'''
+
+	# Remove useless columns
+	metricsFromProgram = metricsFromProgram.drop(['', 'Functions', 'Line of Code', 'Mutants', 'Minimals', '%', 'Equivalents', 'Test Cases'], axis=1)
+	
+	# Renaming columns by removing _F1 at the end
+	metricsFromProgram = metricsFromProgram.rename(lambda x: x.replace('_F1', '') ,axis='columns')
+
+	# Each column_classifier is a value in the dictionary (Example: EM_KNN, MM_RF, etc ...)
+	metricsFromClassifier = pd.DataFrame()
+
+	for program, values in metricsFromProgram.iterrows():
+		for key, value in values.iteritems():
+			classifier = key[key.index('_') + 1 : ]
+			column = 'EQUIVALENT' if key[ : key.index('_')] == 'EM' else 'MINIMAL'
+			f1 = float(value)
+			
+			newDataFrame = pd.DataFrame(data=[[ classifier, column, program, f1 ]], columns=['Classifier', 'Column', 'Program', 'F1'])
+			metricsFromClassifier = metricsFromClassifier.append(newDataFrame)
+
+	if plot:
+		plotMetricsFromClassifier(metricsFromClassifier, possibleClassifiers)
+
+	return metricsFromClassifier
+
+def plotMetricsFromClassifier(metricsFromClassifier, possibleClassifiers):
+	'''
+		Function responsible to show a boxplot for classifiers.
+	'''
+
+	# Set lists with data. For each array, there are several internal arrays containing the F1 values for each program
+	dataMinimal = []
+	dataEquivalent = []
+	for classifier in possibleClassifiers:
+		dataMinimal.append([])
+		dataEquivalent.append([])
+	
+	for row in metricsFromClassifier.itertuples():
+		classifier = getattr(row, 'Classifier')
+		column = getattr(row, 'Column')
+		
+		if column == 'MINIMAL':
+			dataMinimal[possibleClassifiers.index(classifier)].append(getattr(row, 'F1'))
+		elif column == 'EQUIVALENT':
+			dataEquivalent[possibleClassifiers.index(classifier)].append(getattr(row, 'F1'))
+	
+	# Create the figure with axis
+	fig = plt.figure(1, figsize=(9, 6))
+	ax = fig.add_subplot(1, 1, 1)
+
+	# Set the value to be shown as indexes on axis Y
+	ax.set_yticks([value for value in range(0, 101, 10)])
+	ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.75)
+
+	# Set the chart title and axis title
+	#ax.set_title('axes title', fontsize = 14)
+	ax.set_xlabel('\nClassifiers', fontsize = 14)
+	ax.set_ylabel('F1 Score', fontsize = 14)
+
+	# Set the boxplot positions
+	positionsMM = [value for value in range(len(possibleClassifiers) * 2) if value % 2 == 0 ]
+	positionsEM = [value + 0.5 for value in range(len(possibleClassifiers) * 2) if value % 2 == 0 ]
+
+	# Set the boxplots based on a function property defined below
+	bpMM = boxPlotProperties(ax, dataMinimal, positionsMM, '#5975a4')
+	bpEM = boxPlotProperties(ax, dataEquivalent, positionsEM, '#b55d60')
+
+
+	# Calculate the medians
+	minimalMedians = [ np.round(np.median(value), 2) for value in dataMinimal]
+	equivalentMedians = [np.round(np.median(value), 2) for value in dataEquivalent]
+
+	# Calculate the means
+	minimalMeans = [ np.round(np.mean(value), 2) for value in dataMinimal]
+	equivalentMeans = [np.round(np.mean(value), 2) for value in dataEquivalent]
+
+	# Display values in boxplot
+	for tick in range(len(positionsMM)):
+		# Display a median on boxplot top
+		ax.text(positionsMM[tick], 101, minimalMedians[tick], horizontalalignment='center', size='small', color='black', weight='semibold')
+		ax.text(positionsEM[tick], 101, equivalentMedians[tick], horizontalalignment='center', size='small', color='black', weight='semibold')
+
+		#ax.text(positionsMM[tick], minimalMeans[tick] + 0.5, minimalMeans[tick], horizontalalignment='center', size='small', color='black', weight='semibold')
+		#ax.text(positionsEM[tick], equivalentMeans[tick] + 0.5, equivalentMeans[tick], horizontalalignment='center', size='small', color='black', weight='semibold')
+
+			
+	# Set the label between two boxplot
+	#ax.set_xticklabels(list(getFullNamePossibleClassifiers().values())) # Caso for exibir o nome completo do classificador
+	ax.set_xticklabels(possibleClassifiers)
+	ax.set_xticks([value + 0.25 for value in ax.get_xticks() if value % 2 == 0])
+
+	# Set the chart subtitle/legend
+	ax.legend([bpMM["boxes"][0], bpEM["boxes"][0]], ['Minimal Mutants', 'Equivalent Mutants'], loc='lower right')
+
+	# Display chart
+	plt.show()
+
+def boxPlotProperties(ax, data, positions, color):
+	boxprops = dict(facecolor=color, linewidth = 1)
+	medianprops = dict(color='#000000', linewidth = 1)
+	meanprops = dict(color='#000000', linewidth = 1)
+
+	boxplot = ax.boxplot(data, sym='+', positions=positions, patch_artist=True, meanline=True,
+		showmeans=True, labels=possibleClassifiers, boxprops=boxprops, 
+		medianprops=medianprops, meanprops=meanprops)
+
+	return boxplot
+
 if __name__ == '__main__':
 	programsInfo = getProgramsInfo()
 	
@@ -653,10 +764,11 @@ if __name__ == '__main__':
 
 	# ------------------------------------------------------
 	# --- Get informations and write ML metrics for programs
-	#metrics = getMetricsFromPrograms(possibleTargetColumns, possibleClassifiers, programsInfo, bestParameter=True)
+	metrics = getMetricsFromPrograms(possibleTargetColumns, possibleClassifiers, programsInfo, bestParameter=True)
+	
 	#programsBestMetrics = analyzeMetricsFromProgram(metrics)
 
-	#analyzeClassifiers(metrics, possibleClassifiers, plot=True)
+	analyzeClassifiersProgramAProgram(metrics, possibleClassifiers, plot=True)
 
 	# -------------------------------------------
 	# --- Analyze the executions for each program
