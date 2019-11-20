943bbea0b4081f9dba5b5f632c3d1c5911c1159c
ClaudineiBJr <claudineibjr@hotmail.com>
2019-11-20 11:00:32 -0300

    Inclusão de gráfico de barra exibindo o número de programas onde aquele classificador foi o melhor

--- ML/analyzes.py
+++ ML/analyzes.py
@@ -54,12 +54,6 @@ def getProgramsHeader():
 	
 	return columns[0]
 
-def getProgramInfo(programsInfo, programName):
-	if programName in programsInfo.keys():
-		return programsInfo[programName]
-
-	return None
-
 def analyzeResults(targetColumn, classifier, metric = 'F1', findTheBest = True, bestIndex = 0):
 	'''
 		Analyze each of 30 run for each classifier with each target column and calculate the best (calculating the mean) metric for each ones (classifier and target column).
@@ -618,7 +612,7 @@ def getMetricsFromPrograms(possibleTargetColumns, possibleClassifiers, programsI
 
 	return df_programsInfo
 
-def analyzeMetricsFromProgram(metricsFromProgram):
+def analyzeMetricsFromProgram(metricsFromProgram, possibleClassifiers, plot = False):
 	'''
 		Function responsible to verify the programs and identify result of the best classifier
 	'''
@@ -633,14 +627,99 @@ def analyzeMetricsFromProgram(metricsFromProgram):
 	# Iter trough Dataframes and add the max value for each program in the dictionary
 	programsBestMetrics = dict()
 	for program, values in minimalMetrics.iterrows():
-		programsBestMetrics[program] = [max(values), 0]
+		maxValue = max(values)
+		maxIndex = list(values).index(maxValue)
+		bestColumn = str(values.index[maxIndex]).replace('MM_', '').replace('_F1', '')
+		
+		programsBestMetrics[program] = [maxValue, bestColumn, None, None]
 	for program, values in equivalentMetrics.iterrows():
-		programsBestMetrics[program] = [programsBestMetrics[program][0], max(values)]
+		maxValue = max(values)
+		maxIndex = list(values).index(maxValue)
+		bestColumn = str(values.index[maxIndex]).replace('EM_', '').replace('_F1', '')
+		
+		programsBestMetrics[program] = [programsBestMetrics[program][0], programsBestMetrics[program][1], maxValue, bestColumn]
 
-	# Create a dataframe, where the index is the program and has two columns, max F1 for minimals and max f1 for equivalent
-	programsBestMetrics = pd.DataFrame.from_dict(programsBestMetrics, orient='index', columns=['MINIMAL', 'EQUIVALENT'])
+	# Create a dataframe, where the index is the program and has five columns
+	# 	Program name, Max F1 for minimals, Classifier that achieves the best F1 for minimals, Max f1 for equivalents and Classifier that achieves the best F1 for equivalents
+	df_programsBestMetrics = pd.DataFrame()
 
-	return programsBestMetrics
+	for program, value in programsBestMetrics.items():
+		data = [ program, value[0], value[1], value[2], value[3] ]
+		columns = ['Program', 'MINIMAL', 'MM_Classifier', 'EQUIVALENT', 'EM_Classifier']
+		newDataFrame = pd.DataFrame(data=[data], columns=columns)
+		df_programsBestMetrics = df_programsBestMetrics.append(newDataFrame)
+
+	if plot:
+		plotMetricsFromProgram(df_programsBestMetrics, possibleClassifiers)
+
+	return df_programsBestMetrics
+
+def plotMetricsFromProgram(programsBestMetrics, possibleClassifiers):
+	# Set lists with data. For each array, there are several internal arrays containing the F1 values for each program
+	dataMinimal = []
+	dataEquivalent = []
+	for iCount in range(len(possibleClassifiers)):
+		dataMinimal.append(0)
+		dataEquivalent.append(0)
+	
+	for row in programsBestMetrics.itertuples():
+		MM_classifier = getattr(row, 'MM_Classifier')
+		dataMinimal[possibleClassifiers.index(MM_classifier)] = dataMinimal[possibleClassifiers.index(MM_classifier)] + 1
+
+		EM_classifier = getattr(row, 'EM_Classifier')
+		dataEquivalent[possibleClassifiers.index(EM_classifier)] = dataEquivalent[possibleClassifiers.index(EM_classifier)] + 1
+
+	# Create the figure with axis
+	fig = plt.figure(1, figsize=(9, 6))
+	ax = fig.add_subplot(1, 1, 1)
+
+	# Set the value to be shown as indexes on axis Y
+	#ax.set_yticks([value for value in range(0, 101, 10)])
+	ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.75)
+
+	# Set the chart title and axis title
+	#ax.set_title('axes title', fontsize = 14)
+	ax.set_xlabel('\nClassifiers', fontsize = 14)
+	ax.set_ylabel('Number of programs that the classifier was the best', fontsize = 14)
+
+	# Set the boxplot positions
+	width = 0.4  # the width of the bars
+	positionsMM = [value - width / 2 for value in range(len(possibleClassifiers))]
+	positionsEM = [value + width / 2 for value in range(len(possibleClassifiers))]
+
+	# Set the bar chart based on a function property defined below
+	bcMM = barChartProperties(ax, dataMinimal, positionsMM, '#5975a4', width)
+	bcEM = barChartProperties(ax, dataEquivalent, positionsEM, '#b55d60', width)
+
+	# Set the label between two boxplot
+	ax.set_xticklabels(possibleClassifiers)
+	ax.set_xticks([value for value in range(len(possibleClassifiers))])
+
+	# Set the chart subtitle/legend
+	ax.legend([bcMM, bcEM], ['Minimal Mutants', 'Equivalent Mutants'], loc='upper left')
+
+	autolabel(bcMM, ax)
+	autolabel(bcEM, ax)
+
+	fig.tight_layout()
+
+	# Display chart
+	plt.show()
+
+def barChartProperties(ax, data, positions, color, width):
+	barChart = ax.bar(positions, data, width=width, color=color)
+	
+	return barChart
+	
+def autolabel(rects, ax):
+	"""Attach a text label above each bar in *rects*, displaying its height."""
+	for rect in rects:
+		height = rect.get_height()
+		ax.annotate('{}'.format(height),
+					xy=(rect.get_x() + rect.get_width() / 2, height),
+					xytext=(0, 3),  # 3 points vertical offset
+					textcoords="offset points",
+					ha='center', va='bottom')
 
 def analyzeClassifiersProgramAProgram(metricsFromProgram, possibleClassifiers, plot=False):
 	'''
@@ -666,11 +745,11 @@ def analyzeClassifiersProgramAProgram(metricsFromProgram, possibleClassifiers, p
 			metricsFromClassifier = metricsFromClassifier.append(newDataFrame)
 
 	if plot:
-		plotMetricsFromClassifier(metricsFromClassifier, possibleClassifiers)
+		plotClassifiersProgramAProgram(metricsFromClassifier, possibleClassifiers)
 
 	return metricsFromClassifier
 
-def plotMetricsFromClassifier(metricsFromClassifier, possibleClassifiers):
+def plotClassifiersProgramAProgram(metricsFromClassifier, possibleClassifiers):
 	'''
 		Function responsible to show a boxplot for classifiers.
 	'''
@@ -718,8 +797,8 @@ def plotMetricsFromClassifier(metricsFromClassifier, possibleClassifiers):
 	equivalentMedians = [np.round(np.median(value), 2) for value in dataEquivalent]
 
 	# Calculate the means
-	minimalMeans = [ np.round(np.mean(value), 2) for value in dataMinimal]
-	equivalentMeans = [np.round(np.mean(value), 2) for value in dataEquivalent]
+	#minimalMeans = [ np.round(np.mean(value), 2) for value in dataMinimal]
+	#equivalentMeans = [np.round(np.mean(value), 2) for value in dataEquivalent]
 
 	# Display values in boxplot
 	for tick in range(len(positionsMM)):
@@ -739,6 +818,8 @@ def plotMetricsFromClassifier(metricsFromClassifier, possibleClassifiers):
 	# Set the chart subtitle/legend
 	ax.legend([bpMM["boxes"][0], bpEM["boxes"][0]], ['Minimal Mutants', 'Equivalent Mutants'], loc='lower right')
 
+	fig.tight_layout()
+
 	# Display chart
 	plt.show()
 
@@ -754,21 +835,20 @@ def boxPlotProperties(ax, data, positions, color):
 	return boxplot
 
 if __name__ == '__main__':
-	programsInfo = getProgramsInfo()
-	
 	possibleTargetColumns = ['MINIMAL', 'EQUIVALENT']
 	possibleClassifiers = getPossibleClassifiers()
 
 	# --- Analyze the 30 runs and calc statistics informations, like minimum, maximum, median and average
 	#analyzeRuns(possibleTargetColumns, possibleClassifiers)
 
-	# ------------------------------------------------------
-	# --- Get informations and write ML metrics for programs
-	metrics = getMetricsFromPrograms(possibleTargetColumns, possibleClassifiers, programsInfo, bestParameter=True)
+	# ----------------------------------
+	# --- Get informations from programs
+	programsInfo = getProgramsInfo()
+	programsInfo = getMetricsFromPrograms(possibleTargetColumns, possibleClassifiers, programsInfo, bestParameter=True)
 	
-	#programsBestMetrics = analyzeMetricsFromProgram(metrics)
+	programsBestMetrics = analyzeMetricsFromProgram(programsInfo, possibleClassifiers, plot=True)
 
-	analyzeClassifiersProgramAProgram(metrics, possibleClassifiers, plot=True)
+	#analyzeClassifiersProgramAProgram(programsInfo, possibleClassifiers, plot=True)
 
 	# -------------------------------------------
 	# --- Analyze the executions for each program
