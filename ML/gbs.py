6e4430c39d69d13efbb004526bcd437a91a8fe5a
Claudinei Brito Junior <claudineibjr@hotmail.com>
2020-09-21 20:51:24 -0300

    Script Creation / Result Worksheet for GBS

--- ML/gbs.py
+++ ML/gbs.py
@@ -8,12 +8,58 @@ import util
 from util import getPossibleClassifiers, getPossibleTargetColumns
 
 
+def getDropableColumns():
+    possibleDropableColumns = [
+        '_IM_OPERATOR', '_IM_SOURCE_PRIMITIVE_ARC', '_IM_TARGET_PRIMITIVE_ARC',
+        '_IM_DISTANCE_BEGIN_MIN', '_IM_DISTANCE_BEGIN_MAX',
+        '_IM_DISTANCE_BEGIN_AVG', '_IM_DISTANCE_END_MIN',
+        '_IM_DISTANCE_END_MAX', '_IM_DISTANCE_END_AVG', '_IM_COMPLEXITY',
+        '_IM_TYPE_STATEMENT'
+    ]
+    return possibleDropableColumns
+
+
+def getMaxF1(fileName):
+    content = util.splitFileInColumns(fileName, ';')
+    return content[1][4]
+
+
 def experiment():
-    for classifier in getPossibleClassifiers():
-        for targetColumn in getPossibleTargetColumns():
-            print('\n\n', classifier, targetColumn)
-            debug_main(
-                ['', '--column', targetColumn, '--classifier', classifier])
+    baseFolder = '{}/ML/Results'.format(os.getcwd())
+
+    targetColumns = getPossibleTargetColumns()
+    targetColumns.sort()
+
+    classifiers = getPossibleClassifiers()
+    classifiers.sort()
+
+    dropableColumns = getDropableColumns()
+    dropableColumns.sort()
+
+    resultsFile = []
+
+    for targetColumn in targetColumns:
+        for classifier in classifiers:
+            originalFile = '{}/{}/{}.csv'.format(baseFolder, targetColumn,
+                                                 classifier)
+            columnsResult = []
+            f1 = float(getMaxF1(originalFile))
+            columnsResult.append(f1)
+            print('{} - {} - Original: {:.2f}'.format(targetColumn, classifier,
+                                                      f1))
+
+            for column in dropableColumns:
+                columnFile = '{}/{}/{} - gbs_[\'{}\'].csv'.format(
+                    baseFolder, targetColumn, classifier, column)
+                f1 = float(getMaxF1(columnFile))
+                columnsResult.append(f1)
+                print('{} - {} - {}: {:.2f}'.format(targetColumn, classifier,
+                                                    column, f1))
+
+            resultsFile.append([targetColumn, classifier, columnsResult])
+
+    util.writeInCsvFile('{}/ML/Results/gbs.csv'.format(os.getcwd()),
+                        resultsFile)
 
 
 if __name__ == '__main__':
