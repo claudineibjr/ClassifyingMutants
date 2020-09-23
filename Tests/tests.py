8b0f945735be6f9a43f16efdf146b59a2226f9b2
Claudinei Brito Junior <claudineibjr@hotmail.com>
2020-09-23 09:29:01 -0300

    Implementation of T Testand Wilcoxon Test

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,12 +1,17 @@
 # Shapiro-Wilk Test
 # https://machinelearningmastery.com/a-gentle-introduction-to-normality-tests-in-python/
 
+# T Test
+# https://pythonfordatascienceorg.wordpress.com/paired-samples-t-test-python/
+
+# Wilcox Test
+
 import os,sys,inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
-from scipy.stats import shapiro
+from scipy.stats import shapiro, ttest_rel, wilcoxon
 import sys 
 from matplotlib import pyplot
 
@@ -19,7 +24,6 @@ def runShapiroTest(data, alpha):
     print('Statistics: {} | pValue: {} | Is Parametric: {}'.format(stats, pValue, pValue > alpha))
 
     return pValue > alpha
-
 
 def testDataDistribution(metric):
     minimalData, equivalentData = getDataByFile('{}/ML/Results/Summary/Summary_BestClassifiers_All30Runs.csv'.format(os.getcwd()), 'F1')
@@ -42,5 +46,48 @@ def testDataDistribution(metric):
             #  'KNN', 'DT', 'RF', 'SVM', 'LDA' and 'LR' are parametric
             #  'GNB' is nonparametric
 
+def runTTestPaired(dataA, dataB, alpha):
+    stats, pValue = ttest_rel(dataA, dataB)
+    print('Statistics: {} | pValue: {} | There is significative difference: {}'.format(stats, pValue, pValue <= alpha))
+
+    return pValue > alpha
+
+def runWilcoxon(dataA, dataB, alpha):
+    stats, pValue = wilcoxon(dataA, dataB)
+    print('Statistics: {} | pValue: {} | There is significative difference: {}'.format(stats, pValue, pValue <= alpha))
+
+    return pValue > alpha
+
+def testHypothesis(metric):
+    minimalData, equivalentData = getDataByFile('{}/ML/Results/Summary/Summary_BestClassifiers_All30Runs.csv'.format(os.getcwd()), 'F1')
+
+    # ========================================
+    # ===== Hypothesis Testing ===============
+    # ========================================   
+    for model, data in [['MINIMAL', minimalData], ['EQUIVALENT', equivalentData]]:
+        print(int(len(model) + 20 + 2) * '=')
+        print('===== {} ==============='.format(model))
+        print(int(len(model) + 20 + 2) * '=')
+        
+        allClassifiers = getPossibleClassifiers()
+        classifiersToBeTested = getPossibleClassifiers()
+
+        for classifierA in allClassifiers:
+            if len(classifiersToBeTested) > 0:
+                classifiersToBeTested.remove(classifierA)
+            
+            for classifierB in classifiersToBeTested:
+                print('{} x {}'.format(classifierA, classifierB))
+                dataA = data.query('Classifier == \'{}\''.format(classifierA))
+                dataB = data.query('Classifier == \'{}\''.format(classifierB))
+                if (classifierA == 'GNB' or classifierB == 'GNB'):
+                    runWilcoxon(dataA[metric], dataB[metric], 0.05)
+                else:
+                    runTTestPaired(dataA[metric], dataB[metric], 0.05)
+                print('')
+        print('')
+
+
 if __name__ == '__main__':
-    testDataDistribution('F1')
+    # testDataDistribution('F1')
+    # testHypothesis('F1')
